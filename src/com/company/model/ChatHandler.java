0a3069979bf915d416abd189197892998a3f4dfc
Kopranych <kopranych@gmail.com>
2017-09-27 22:45:52 +0300

    add code class ChatHandler

--- src/com/company/model/ChatHandler.java
+++ src/com/company/model/ChatHandler.java
@@ -1,11 +1,34 @@
 package com.company.model;
 
+import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class ChatHandler extends Thread {
-    Socket socket;
+    protected Socket socket;
+    protected DataInputStream dis;
+    protected DataOutputStream dos;
+    protected boolean isOn;
 
-    public ChatHandler(Socket socket) {
+    protected static List<ChatHandler> listHandler = Collections.synchronizedList(new ArrayList<>());
+
+    public ChatHandler(Socket socket) throws IOException {
         this.socket = socket;
+        dis = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+        dos = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+    }
+
+    public void run(){
+        isOn = true;
+        listHandler.add(this);
+        while (isOn) {
+            try {
+                dis.readUTF();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
